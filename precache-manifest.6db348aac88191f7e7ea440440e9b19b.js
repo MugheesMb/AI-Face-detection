d4da7838c1ea051b63ff10fe479c1e94ee39c119
mugymb <Mr.Mb@tuta.io>
2020-04-27 01:51:42 +0500

    Updates

---
+++ precache-manifest.6db348aac88191f7e7ea440440e9b19b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "437dc2eb8b2e1598fa734441e7b4991f",
+    "url": "/AI-Face-detection/index.html"
+  },
+  {
+    "revision": "a63f6293e614fa306d65",
+    "url": "/AI-Face-detection/static/css/2.c40930e6.chunk.css"
+  },
+  {
+    "revision": "a91ad0a2f151b380eb54",
+    "url": "/AI-Face-detection/static/css/main.d8a4e95d.chunk.css"
+  },
+  {
+    "revision": "a63f6293e614fa306d65",
+    "url": "/AI-Face-detection/static/js/2.75535d8b.chunk.js"
+  },
+  {
+    "revision": "ca5b2c05011b8c026390333604cd2198",
+    "url": "/AI-Face-detection/static/js/2.75535d8b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a91ad0a2f151b380eb54",
+    "url": "/AI-Face-detection/static/js/main.1f1eedc4.chunk.js"
+  },
+  {
+    "revision": "c25337ae983f0892da10",
+    "url": "/AI-Face-detection/static/js/runtime-main.042874e8.js"
+  }
+]);
